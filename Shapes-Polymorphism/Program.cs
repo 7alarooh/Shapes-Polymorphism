9f611d6c82779da7cedfd06189cc955321581942
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-30 15:42:59 +0400

    first commit

---
+++ Shapes-Polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Shapes_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
