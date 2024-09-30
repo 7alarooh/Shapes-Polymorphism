f512774a5c0797d9217843ecfa3357487df71236
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-30 16:23:05 +0400

    structure to shapes calculation

--- Shapes-Polymorphism/Program.cs
+++ Shapes-Polymorphism/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Shapes_Polymorphism
+﻿using System;
+namespace Shapes_Polymorphism
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //create instances of shapes
+            shape c = new circle();
+            shape R = new rectangle();
+            shape t = new triangle();
+            shape s = new square();
+
+            c.area();
+            R.area();
+            s.area();
+            t.area();
+
         }
+        //Base class
+        public class shape
+        {
+            protected double lentgh;
+            protected double width;
+
+            public virtual double area() { return 0; }
+        }
+        //derived class: Circle
+        public class circle:shape 
+        {
+            private double radius;
+        }
+        public class rectangle : shape { }
+        public class triangle : shape { }
+
+        public class square : shape { }
+
     }
 }
